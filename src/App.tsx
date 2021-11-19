cd20c45a85f035ee94fe81ce24466a8c84aaa643
filipmarkoski <filip.markoski45@gmail.com>
2021-11-19 16:16:32 +0100

    Working on Collapse

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css';
+// import './App.css';
 import {Tasks} from './features/tasks/Tasks';
-import Grid from '@mui/material/Grid'
+// import Grid from '@mui/material/Grid'
 
 function App() {
     return (
-        <Grid container
-              spacing={0}
-              direction="column"
-              alignItems="center"
-              justifyContent="center"
-              style={{minHeight: '100vh'}}>
-            <div className="App">
-                <Tasks />
-            </div>
-        </Grid>
+        // <div className='App'>
+            <Tasks/>
+        // </div>
+        // <Grid container
+        //       spacing={0}
+        //       direction='row'
+        //       alignItems="center"
+        //       justifyContent="center"
+        //       display='flex' flex='1' height='100%'
+        //       style={{minHeight: '100vh'}}>
+        //
+        // </Grid>
     );
 }
 
--- src/features/tasks/Tasks.tsx
+++ src/features/tasks/Tasks.tsx
@@ -10,11 +10,18 @@ import Box from '@mui/material/Box';
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Grid from '@mui/material/Grid'
-import Autocomplete from '@mui/material/Autocomplete'
+import Collapse from '@mui/material/Collapse'
 import DeleteIcon from '@mui/icons-material/Delete';
 import {useEffect, useState} from "react";
 import {red} from '@mui/material/colors';
 import {MouseEvent} from 'react';
+import {ChangeEvent} from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import {Accordion, AccordionDetails, AccordionSummary, Paper, Stack} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CloseIcon from '@mui/icons-material/Close';
 
 export interface ITask {
     ID: number,
@@ -26,14 +33,15 @@ export interface ITask {
     UUID: string
 }
 
-type TaskParcel = Pick<ITask, "Name" | "Description">;
+type TaskParcel = Pick<ITask, 'Name' | 'Description'>;
 
 export function Tasks() {
-    const [checked, setChecked] = React.useState([0]);
-    const [task, setTask] = React.useState('');
+    const [checked, setChecked] = useState([0]);
+    const [task, setTask] = useState<ITask>();
     const [tasks, setTasks] = useState<ITask[]>([]);
+    const [featuredTasks, setFeaturedTasks] = useState<ITask[]>([]);
+    const [open, setOpen] = useState(false);
 
-    const optionsTask = tasks.map(task => task.Name);
 
     useEffect(() => {
         // fetch('http://127.0.0.1:5000/api/tasks')
@@ -44,14 +52,19 @@ export function Tasks() {
             }
         })
             .then(response => response.json())
-            .then(data => setTasks(data))
+            .then(tasks => {
+                setTasks(tasks);
+                setFeaturedTasks(tasks);
+                let task: ITask = tasks[0];
+                setTask(task);
+            })
             .catch(error => console.error(error));
-    }, [setTasks]);
+    }, [setTasks, setTask]);
 
     const postTask = () => {
         const taskParcel: TaskParcel = {
-            Name: task,
-            Description: `${task} Description`
+            Name: task?.Name ?? 'TaskName',
+            Description: `${'task?.Name'} Description`
         }
 
         fetch('http://127.0.0.1:5000/api/tasks/', {
@@ -79,7 +92,7 @@ export function Tasks() {
     // : ((event: MouseEvent) => void)
     const toggle = (event: MouseEvent, taskID: number) => {
         event.preventDefault();
-
+        setOpen(true);
         const currentIndex = checked.indexOf(taskID);
         const newChecked = [...checked];
 
@@ -88,6 +101,9 @@ export function Tasks() {
         } else {
             newChecked.splice(currentIndex, 1);
         }
+
+        let selectedTask: ITask = tasks.filter(task => task.ID === taskID)[0];
+        setTask(selectedTask);
 
         if (event.shiftKey) {
             console.log('Shift-Click recognized.');
@@ -104,80 +120,153 @@ export function Tasks() {
         setChecked(newChecked);
     };
 
+    const handleSearch = (event: ChangeEvent) => {
+        event.preventDefault();
+        const target = event.target as HTMLTextAreaElement;
+        let value = target.value;
+        if (value === null || value === '') {
+            setFeaturedTasks(tasks);
+            return;
+        }
+        setFeaturedTasks(tasks.filter(task => task.Name.includes(value)));
+    };
 
     return (
-        <Box
-            sx={{
-                border: 1,
-                borderRadius: 1,
-                padding: 1
-            }}
+
+        <Grid container
+              direction='row'
+              justifyContent='center'
+              alignItems='stretch'
+              sx={{
+                  minHeight: 400,
+                  margin: 16,
+                  background: 'yellow'
+              }}
         >
 
-            <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={optionsTask}
-                sx={{width: 300}}
-                renderInput={(params) => <TextField {...params} label="Search Tasks"/>}
-            />
-
-            <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
-                {tasks.map((task) => {
-                    const labelId = `checkbox-list-label-${task.ID}`;
-
-                    return (
-                        <ListItem
-                            key={task.ID}
-                            onClick={(event) => toggle(event as MouseEvent, task.ID)}
-                            secondaryAction={
-                                <IconButton edge="end" aria-label="comments"
-                                            onClick={() => deleteTask(task.ID)}>
-                                    <DeleteIcon/>
-                                </IconButton>
-                            }
-                            disablePadding
-                        >
-
-                            <ListItemButton dense>
-                                <ListItemIcon>
-                                    <Checkbox
-                                        edge="start"
-                                        checked={checked.indexOf(task.ID) !== -1}
-                                        tabIndex={-1}
-                                        disableRipple
-                                        inputProps={{'aria-labelledby': labelId}}
-                                        sx={{
-                                            color: red[800],
-                                            '&.Mui-checked': {
-                                                color: red[600],
-                                            },
-                                        }}
-                                    />
-                                </ListItemIcon>
-                                <ListItemText id={labelId} primary={`${task.Name}`}/>
-                            </ListItemButton>
-                        </ListItem>
-                    );
-                })}
-            </List>
-
-            <Box
-                component="form"
-                autoComplete="off"
+            <Grid
+                item xs={6}
+                sx={{
+                    border: 1,
+                    borderRadius: 1,
+                    padding: 1,
+                    bgcolor: 'orange'
+                }}
+                style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}
             >
-                <Grid container
-                      spacing={0}
-                      direction="row"
-                      alignItems="center"
-                      justifyContent="center">
-                    <TextField id="outlined-basic" label="Add Task" variant="outlined" size="small" color='success'
-                               onChange={(e) => setTask(e.target.value)}/>
-                    <Button variant="outlined" color='success' size="medium" sx={{ml: 1, pt: 0.85, pb: 0.85}}
-                            onClick={postTask}>Add Task</Button>
-                </Grid>
-            </Box>
+                <TextField id="standard-basic" variant="standard"
+                           onChange={handleSearch}
+                           label={'Search the tasks...'}
+                />
+                <List sx={{bgcolor: 'background.paper'}}>
+                    {featuredTasks.map((task) => {
+                        const labelId = `checkbox-list-label-${task.ID}`;
 
-        </Box>
+                        return (
+                            <ListItem
+                                key={task.ID}
+                                onClick={(event) => toggle(event as MouseEvent, task.ID)}
+                                secondaryAction={
+                                    <IconButton edge="end" aria-label="comments"
+                                                onClick={() => deleteTask(task.ID)}>
+                                        <DeleteIcon/>
+                                    </IconButton>
+                                }
+                                disablePadding
+                            >
+
+                                <ListItemButton dense>
+                                    <ListItemIcon>
+                                        <Checkbox
+                                            edge="start"
+                                            checked={checked.indexOf(task.ID) !== -1}
+                                            tabIndex={-1}
+                                            disableRipple
+                                            inputProps={{'aria-labelledby': labelId}}
+                                            sx={{
+                                                color: red[800],
+                                                '&.Mui-checked': {
+                                                    color: red[600],
+                                                },
+                                            }}
+                                        />
+                                    </ListItemIcon>
+                                    <ListItemText id={labelId} primary={`${task.Name}`}/>
+                                </ListItemButton>
+                            </ListItem>
+                        );
+                    })}
+                </List>
+                {/* Add Task */} <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Add Task</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+
+                    <Box
+                        component="form"
+                        autoComplete="off"
+                    >
+                        <Grid container
+                              spacing={0}
+                              direction="row"
+                              alignItems="center"
+                              justifyContent="center">
+                            <TextField id="outlined-basic" label="Task Name..." variant="outlined" size="small"
+                                       color='success'
+                                       onChange={(e) => console.log(e.target.value)}/>
+                            <Button variant="outlined" color='success' size="medium"
+                                    sx={{ml: 1, pt: 0.85, pb: 0.85}}
+                                    onClick={postTask}>Add Task</Button>
+                        </Grid>
+                    </Box>
+                </AccordionDetails>
+            </Accordion>
+
+            </Grid>
+
+            {/* Card */}
+            <Grid item xs={6} component={Card} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'stretch'}}
+                  sx={{bgcolor: 'red',}}>
+                <Collapse in={open} timeout="auto">
+                    <CardContent>
+                        <Stack direction="row"
+                               justifyContent="space-between"
+                               alignItems="baseline"
+                               spacing={2}
+                        style={{position: 'fixed', margin: 0,
+    top: 'auto',}}>
+                            <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                Task #{task?.ID}
+                            </Typography>
+                            <IconButton onClick={() => {
+                                setOpen(false)
+                            }}>
+                                <CloseIcon/>
+                            </IconButton>
+
+                        </Stack>
+
+                        <Typography variant="h5" component="div">
+                            {task?.Name}
+                        </Typography>
+                        <Typography sx={{mb: 1.5}} color="text.secondary">
+                            {task?.Description}
+                        </Typography>
+                        <Typography variant="body2">
+                            {task?.DateCreated}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Grid>
+
+
+        </Grid>
+
+
     );
 }
